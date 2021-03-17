406e783a84710060cfe1c39a6e71ab18f44b4e0c
Matthew Thomas <matthew@mwt.me>
2021-03-17 12:05:38 -0500

    rewrite for MathJaX 3

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,22 +1,14 @@
 (function() {
-    function t(t) {
-        if (t.MathJax === undefined) {
-            t.MathJax = {
+        if (window.MathJax === undefined) {
+            window.MathJax = {
                 tex: {
-                inlineMath: [['$', '$'], ['\\(', '\\)']]
+                    inlineMath: [['$', '$'], ['\\(', '\\)']]
                 }
-            }
-            const i = document.createElement('script')
-            i.src = "//cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
-            i.async = true
-            t.document.body.appendChild(i)
+            };
+            const i = document.createElement('script');
+            i.src = '//cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+            i.async = true;
+            window.document.body.appendChild(i)
         }
     }
-    var a = document.getElementsByTagName('iframe');
-    t(window);
-    for (n = 0; n < a.length; n++) {
-        i = a[n].contentWindow || a[n].contentDocument;
-        if (!i.document) i = i.parentNode;
-        t(i)
-    }
-})();
+)();
